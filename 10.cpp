2d1d97a93674026513b368eb4e7e0f9c07f57db3
nhuquynh207 <tentaikhoancuato@gmail.com>
2025-10-29 23:37:36 +0700

    Add files via upload

--- 10.cpp
+++ 10.cpp
@@ -13,14 +13,13 @@ int main ( ){
 		printf("-");
 		a = abs(a);
 	}
-	int t = a; // du`ng dê? check lu~y thu`a	
+	int t = a; 
 	while ( t >=10){
-		t /=10; //ti´nh toa´n t co´ mâ´y chu~ sô´ thi` k se~ tuong u´ng vo´i bâ´y nhiêu if 1234 <=> k = 1000
-		k*=10; // k tang dâ`n dê? ti´nh toa´n dê´n ca´c sô´ tiê´p theo		
+		t /=10; 
+		k*=10; 
 	} printf("Cac chu so tuong ung:");
 	
 	while ( k>= 1){
-	    // chu~ sô ngoa`i cu`ng bên tra´i tuong u´ng vo´i
 	    int digit = a / k;
 	    printf("%d",digit);
 	    a %=k;
